a7fd58ee55fd763243840e75374278b721c937e9
Guillermo <guillermo.albert@outlook.com>
2025-01-02 13:14:10 +0100

    creacion del proyecto spring

---
+++ gestion-finanzas-backend/src/main/java/com/gestion/finanzas/GestionFinanzasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.finanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFinanzasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFinanzasBackendApplication.class, args);
+	}
+
+}
